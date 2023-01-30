f335de869eefb2da1a0a3444c83bf3ae3954e4e5
prash <prash@192.168.0.42>
2023-01-30 00:34:08 -0500

    Commit code

---
+++ src/whooktest/whookcode.java
@@ -0,0 +1,10 @@
+package whooktest;
+
+public class whookcode {
+
+	public static void main(String[] args) {
+		System.out.println("Whook triger testing");
+		System.out.println("Checking");
+	}
+
+}
